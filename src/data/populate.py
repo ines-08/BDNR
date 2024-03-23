24f1326a32a5fe6cae6770f41058914ccf9cd473
Fábio Sá <up202007658@edu.fe.up.pt>
2024-03-23 15:57:30 +0000

    Improvement: populate using threads

--- src/data/populate.py
+++ src/data/populate.py
@@ -1,18 +1,35 @@
 import sys
 import json
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 
-ETCD_NODE = "etcd1"
+ETCD_NODES = ["etcd1", "etcd2", "etcd3", "etcd4", "etcd5"]
 
-if len(sys.argv) != 2:
-    print("Usage: python populate.py <INPUT>")
-    sys.exit(1)
+def process_data_chunk(keys, values, node):
+    for key, value in zip(keys, values):
+        command = f"ETCDCTL_API=3 etcdctl put {key} '{json.dumps(value)}'"
+        subprocess.run(["docker-compose", "exec", node, "sh", "-c", command], check=True)
 
-JSON_FILE = sys.argv[1]
-with open(JSON_FILE, "r") as file:
-    data = json.load(file)
-    file.close()
+def main():
 
-for key, value in data.items():
-    command = f"ETCDCTL_API=3 etcdctl put {key} '{json.dumps(value)}'"
-    subprocess.run(["docker-compose", "exec", ETCD_NODE, "sh", "-c", command])
+    if len(sys.argv) != 2:
+        print("Usage: python populate.py <INPUT>")
+        sys.exit(1)
+
+    JSON_FILE = sys.argv[1]
+    with open(JSON_FILE, "r") as file:
+        data = json.load(file)
+
+    keys = list(data.keys())
+    values = list(data.values())
+
+    chunk_size = len(keys) // len(ETCD_NODES)
+    key_chunks = [keys[i:i+chunk_size] for i in range(0, len(keys), chunk_size)]
+    value_chunks = [values[i:i+chunk_size] for i in range(0, len(values), chunk_size)]
+
+    with ThreadPoolExecutor(max_workers=len(ETCD_NODES)) as executor:
+        for i, node in enumerate(ETCD_NODES):
+            executor.submit(process_data_chunk, key_chunks[i], value_chunks[i], node)
+
+if __name__ == "__main__":
+    main()
