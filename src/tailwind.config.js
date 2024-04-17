52d313075ed701d64a136c7018c4b29b486445de
José Albano Gaspar <up202008561@edu.fe.up.pt>
2024-04-17 14:53:10 +0100

    tailwind setup

---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/*.{ejs}",
+    "./public/**/*.{html,js,css} ",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
