3b213dceb55cab5bdd01d10e506608bb86a19804
José Albano Gaspar <up202008561@edu.fe.up.pt>
2024-04-17 17:38:49 +0100

    tailwind first version

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./views/*.{ejs}",
-    "./public/**/*.{html,js,css} ",
+    "./views/**/*.ejs",
   ],
   theme: {
     extend: {},
