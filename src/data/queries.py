c7c36a53817405349a420f247b79222b543619d2
Fábio Sá <up202007658@edu.fe.up.pt>
2024-04-17 16:11:25 +0100

    Manual queries: using patterns

--- src/data/queries.py
+++ src/data/queries.py
@@ -1,4 +1,3 @@
-from email.policy import default
 import subprocess
 
 # Please change this in PROD environment
@@ -10,20 +9,29 @@ ETCD_NODES = {
     "etcd5" : "http://localhost:2385",     
 }
 
-DEFAULT_NODE = ETCD_NODES['etcd1']
+DEFAULT_NODE = 'etcd1'
+DEFAULT_ADDRESS = ETCD_NODES[DEFAULT_NODE]
 
-QUERIES = {
-    "Getting all ticket types": "put some 'thing'",
-}
+QUERIES = [
+    { "description": "A simple put", "code": "put some thing", "output": False },
+    { "description": "Getting all ticket types", "code": "get ticket:types", "output": True }
+]
 
-def run_query(query):
-    print(query)
-    subprocess.run(["docker-compose", "-f", "docker-compose-dev.yml", "exec", DEFAULT_NODE, "sh", "-c", "ETCDCTL_API=3", "etcdctl", "--endpoints={DEFAULT_NODE}", query], check=True)
+def run_query(query, output=True):
+    command = f"ETCDCTL_API=3 etcdctl --endpoints={DEFAULT_ADDRESS} {query}"
+    subprocess.run([
+        "docker-compose", "-f", "docker-compose-dev.yml", 
+        "exec", DEFAULT_NODE, "sh", "-c", command
+    ], 
+        stdout=subprocess.DEVNULL if not output else None, 
+        stderr=subprocess.DEVNULL if not output else None, 
+        check=True
+    )
 
 def run():
-    for description, query in QUERIES.items():
-        print(description)
-        run_query(query)
+    for query in QUERIES:
+        print(query['description'])
+        run_query(query['code'], query['output'])
 
 if __name__ == '__main__':
     run()
